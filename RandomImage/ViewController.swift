bddbb222a2591e31196e7ea09f56c70829d1fb59
Cameron Braverman <bravermancj@gmail.com>
2018-04-06 17:48:24 -0400

    Initial Commit

---
+++ RandomImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomImage
+//
+//  Created by Cameron Braverman on 4/6/18.
+//  Copyright © 2018 Cameron Braverman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
